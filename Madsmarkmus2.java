e1e0d9b6a4037f180ea7e7ff8acfd231fcdfd7e7
Madsmarkmus <75371111+Madsmarkmus@users.noreply.github.com>
2020-12-12 16:47:36 +0100

    Update Madsmarkmus2.java

--- Madsmarkmus2.java
+++ Madsmarkmus2.java
@@ -5,29 +5,31 @@ public class Madsmarkmus2 extends JuniorRobot
 {
 	boolean erHanDer = false;
 	public void run() {
+	
 		if (robotY > 20) {
-		turnTo(180);
-		ahead(robotY-20);
-		turnTo(90);
-		turnGunTo(90);
+			turnTo(180);
+			ahead(robotY-20);
+			turnTo(90);
+			turnGunTo(90);
 		}
-
-		ahead(fieldWidth-robotY-20);
-		if (robotX > fieldWidth-30) {
+	
+		ahead(fieldWidth-robotX-20);
+		
+		if (robotX == fieldWidth-20) {
 			turnGunTo(0);
-			if (erHanDer == true) {
-				fire(3);
-			}
+			if (erHanDer == true)
+				fire(3);		
+			turnGunTo(90);
 		}
-		turnGunTo(90);
-		back((fieldWidth)-20-20);	
-		erHanDer = false;
+		
+		back(robotX-20);	
 	}
 	
 	public void onScannedRobot() {
-		erHanDer = true;
-		if (scannedHeading == 270 || scannedHeading == 180) {
-			fire(3);
+		if (robotY == 20) {
+			erHanDer = true;
+			if (heading == 90);
+				fire(3);
 		}
 	}
 }
